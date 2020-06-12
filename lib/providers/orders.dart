57b84819083a7007dedd4de5a1e7942c9d9f610e
Kaushal Wagle <kaushalwagle@gmail.com>
2020-06-12 23:34:31 +0545

    added orders using the order now with a loading spinnner while processing order

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/http_exception.dart';
+
 import './cart.dart';
 
 class OrderItem with ChangeNotifier {
@@ -22,16 +28,37 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  addOrder(List<CartItem> cartProducts, double total) {
-    _orders.insert(
-      0,
-      OrderItem(
-        id: DateTime.now().toString(),
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    const url = 'https://flutter-shop-dca7d.firebaseio.com/orders.json';
+    final timestamp = DateTime.now();
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'amount': total,
+          'products': cartProducts
+              .map((cp) => {
+                    'id': cp.id,
+                    'title': cp.title,
+                    'quantity': cp.quantity,
+                    'price': cp.price
+                  })
+              .toList(),
+          'dateTime': timestamp.toIso8601String(),
+        }),
+      );
+      final newOrder = OrderItem(
+        id: json.decode(response.body)['name'],
         amount: total,
         products: cartProducts,
-        dateTime: DateTime.now(),
-      ),
-    );
+        dateTime: timestamp,
+      );
+      _orders.add(newOrder);
+      notifyListeners();
+    } catch (error) {
+      print(error.toString());
+      throw HttpException('Could not register an order.');
+    }
     notifyListeners();
   }
 }
